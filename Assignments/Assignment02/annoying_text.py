16f823e89b2d70c85d795cfc1abaeb237bd43be9
Littleflower775 <jodie.sa.ip@gmail.com>
2024-09-05 19:14:06 +1000

    Delete the old version

--- Assignments/Assignment02/annoying_text.py
+++
@@ -1,6 +0,0 @@
-original_text = "Ice Cream! "
-shouting_text = original_text.upper()
-annoying_text = 10 * shouting_text
-print(f"My annoying text is {annoying_text}")
-print("")
-print("It has", len(f"My annoying text is {annoying_text}"), "characters.") 
