1346dd8c79b1ece8a943717755bf0d3949ea31e5
Littleflower775 <jodie.sa.ip@gmail.com>
2024-09-05 19:12:52 +1000

    Assignment2

---
+++ Assignments/Assignment02/annoying_text_v2.py
@@ -0,0 +1,7 @@
+original_text = "Ice Cream! "
+shouting_text = original_text.upper()
+annoying_text = 10 * shouting_text
+annoying_text_count = len(f"My annoying text is {annoying_text}")
+print(f"""My annoying text is {annoying_text}
+      
+It has {annoying_text_count} characters.""") 
