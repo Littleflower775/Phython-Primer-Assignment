55e17928e914488a179ebc7652ba09749fa5f9c5
Littleflower775 <jodie.sa.ip@gmail.com>
2024-09-19 00:05:47 +1000

    new_file

---
+++ Lesson04/lists_intro.py
@@ -0,0 +1,23 @@
+pets = ['bird', 'cat', 'dog']
+print('dog' in pets)
+print('fish' not in pets)
+
+colours = ['blue', 'green', 'purple', 'orange']
+print(colours[1:3])
+
+pets = ['bird', 'cat', 'dog', 'hamster', 'rabbit' ,'frog']
+print(pets[:3])
+print(pets[2:])
+print(pets[2:5])
+print(pets[4:])
+print(pets[-1])
+
+colours = ['red', 'yellow', 'green', 'blue']
+print(colours[1:3])
+print(colours[:5])
+print(colours[3:])
+print(colours[-2])
+print(colours[-2:])
+
+numbers = [1, 2, 3, 4, 5]
+print(3 in numbers)
