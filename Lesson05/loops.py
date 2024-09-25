6e723a615be93bc222b0ef83e20e7caf4d827c9a
Littleflower775 <jodie.sa.ip@gmail.com>
2024-09-25 21:50:38 +1000

    Adding 2nd file

---
+++ Lesson05/loops.py
@@ -0,0 +1,51 @@
+# i = 0
+
+# while (i < 3):
+#     print(i)
+#     i = i + 1
+
+# pets = ['bird', 'cat', 'dog', 'rabbit']
+
+# i = 0
+# while (i < len(pets)):
+#     print(pets[i])
+#     i = i + 1
+
+# numbers = [7,15,4]
+
+# for number in numbers:
+#     print(number)
+
+# for i in range(3):
+#     print(i)
+
+# password = 'python'
+# secret_message = 'Code Like A Girl'
+
+# user_password = input('Enter your password: ')
+
+# while(user_password != password):
+#     user_password = input('Enter your password: ')
+# print(secret_message)
+
+name = 'Code Like A Girl'
+for letter in name:
+    print(letter + '?')
+
+for i in range(10):
+    print(i)
+
+i = 0
+while(i < len(name)):
+    print(name[i] + '.')
+    i = i + 1
+
+i = 0
+while (i < 3):
+    print(f'Loop {i}')
+    i = i + 1
+
+pets = ['bird', 'cat', 'dog']
+
+for pet in pets:
+    print(pet)
